5d411d87f68d63c011ac00976c3915808c1ec999
AgustinSosa <sosaagustin1991@gmail.com>
2023-06-27 10:45:56 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Lentitud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRuta = new System.Windows.Forms.Button();
+            this.lbArchivos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRuta
+            // 
+            this.btnRuta.Location = new System.Drawing.Point(12, 12);
+            this.btnRuta.Name = "btnRuta";
+            this.btnRuta.Size = new System.Drawing.Size(272, 48);
+            this.btnRuta.TabIndex = 0;
+            this.btnRuta.Text = "Ruta";
+            this.btnRuta.UseVisualStyleBackColor = true;
+            this.btnRuta.Click += new System.EventHandler(this.btnRuta_Click);
+            // 
+            // lbArchivos
+            // 
+            this.lbArchivos.FormattingEnabled = true;
+            this.lbArchivos.ItemHeight = 20;
+            this.lbArchivos.Location = new System.Drawing.Point(12, 210);
+            this.lbArchivos.Name = "lbArchivos";
+            this.lbArchivos.Size = new System.Drawing.Size(272, 364);
+            this.lbArchivos.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1310, 586);
+            this.Controls.Add(this.lbArchivos);
+            this.Controls.Add(this.btnRuta);
+            this.Name = "Form1";
+            this.Text = "Lentitud del centro";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRuta;
+        private System.Windows.Forms.ListBox lbArchivos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Lentitud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRuta_Click(object sender, EventArgs e)
+        {
+            string RutaDirectorio = null;
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK) 
+            {
+                RutaDirectorio = fbd.SelectedPath;
+            }
+
+            DirectoryInfo di = new DirectoryInfo(@RutaDirectorio);
+
+            foreach (var item in di.GetFiles())
+            {
+                lbArchivos.Items.Add(item.Name);
+            }
+        }
+    }
+}
